546f1159d8eff5f11bb898aa8c16e9a9f0f10a57
Rico Putra Pradana <pradanaricoputra@gmail.com>
2024-07-06 21:38:09 +0700

    feat: implement sign up feature

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { signup } from "../../server/auth";
+import { useFormState } from "react-dom";
+
+export default async function Page() {
+  // const [] = useFormState(signup)
+	return (
+		<>
+			<h1>Create an account</h1>
+			<form action={signup}>
+        <label htmlFor="name">Name</label>
+				<input name="name" id="name" />
+				<br />
+
+        <label htmlFor="email">Email</label>
+				<input type="email" name="email" id="email" />
+				<br />
+
+				<label htmlFor="username">Username</label>
+				<input name="username" id="username" />
+				<br />
+
+				<label htmlFor="password">Password</label>
+				<input type="password" name="password" id="password" />
+				<br />
+				<button>Continue</button>
+
+        <a href="https://google.com">Click here to goole</a>
+			</form>
+		</>
+	);
+}
---
+++ src/server/auth/signup.ts
@@ -0,0 +1,102 @@
+"use server";
+
+import { ActionResult } from "next/dist/server/app-render/types";
+import { hash } from "@node-rs/argon2";
+import { cookies } from "next/headers";
+import { generateIdFromEntropySize } from "lucia";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+import { eq, or } from "drizzle-orm";
+
+import { lucia } from "../../lib/auth"
+import db from "../../db";
+import { users } from "../../db/schema";
+
+const signupFormSchema = z.object({
+  name: z.string().min(1).max(256),
+  email: z.string().email(),
+
+  // username must be between 4 ~ 31 characters,
+  // and only consists of lowercase letters, 0-9, -, and _
+  username: z.string().min(4).max(31).regex(/^[a-z0-9_-]+$/),
+
+  // password must be between 6 ~ 255 characters
+  password: z.string().min(6).max(255),
+});
+
+export async function signup(formData: FormData): Promise<ActionResult> {
+  const formValues = {
+    name: formData.get("name"),
+    email: formData.get("email"),
+    username: formData.get("username"),
+    password: formData.get("password"),
+  };
+
+  // validate the form data
+  const parsedForm = signupFormSchema.safeParse(formValues);
+  if (!parsedForm.success) {
+    return {
+      error: "Invalid form data",
+      details: parsedForm.error.errors
+    };
+  }
+
+  const {
+    name,
+    username,
+    password,
+    email
+  } = parsedForm.data;
+
+	const passwordHash = await hash(password, {
+    // The amount of memory to be used by the hash function, in kilobytes
+		memoryCost: 19456,
+
+    // The amount of passes (iterations) used by the hash function.
+    // It increases hash strength at the cost of time required to compute.
+		timeCost: 2,
+
+    // The hash length is the length of the hash function output in bytes
+		outputLen: 32,
+
+    // The amount of threads to compute the hash on.
+    // Each thread has a memory pool with memoryCost size
+		parallelism: 1
+	});
+
+	const userId = generateIdFromEntropySize(10); // 16 characters long
+
+  // check if email OR username is already used
+  const user = await db
+    .select()
+    .from(users)
+    .where(
+      or(
+        eq(users.email, email),
+        eq(users.username, username)
+      )
+    )
+
+  // username or email is already used
+  if (user.length > 0) {
+    return {
+      error: "Username or email is already used",
+    };
+  }
+
+  // store the user in the database
+  await db.insert(users).values({
+    id: userId,
+    name,
+    email,
+    username,
+    password: passwordHash
+  });
+
+	const session = await lucia.createSession(userId, {});
+
+	const sessionCookie = lucia.createSessionCookie(session.id);
+
+  cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+	return redirect("/");
+}
