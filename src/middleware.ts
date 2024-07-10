1c00b59c9141d7bf001b1a5e5fb8d7551d0f93ac
Rico Putra Pradana <pradanaricoputra@gmail.com>
2024-07-11 06:43:04 +0700

    feat: redirect the authenticated user to Dashboard when trying to access the root path

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import EnumRoutes from "./enums/EnumRoutes";
 
 export function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
@@ -7,9 +8,9 @@ export function middleware(req: NextRequest) {
   /**
    * Redirects the user to the authenticated page if they are already authenticated.
    */
-  const PROTECTED_ROUTES = ["/login", "/signup"];
-  if (PROTECTED_ROUTES.includes(pathname) && req.cookies.get("auth_session")) {
-    return NextResponse.redirect(new URL("/", req.url));
+  const PROTECTED_ROUTES = [EnumRoutes.LOGIN, EnumRoutes.SIGNUP, "/"];
+  if (PROTECTED_ROUTES.includes(pathname as EnumRoutes) && req.cookies.get("auth_session")) {
+    return NextResponse.redirect(new URL(EnumRoutes.DASHBOARD, req.url));
   }
 
   return NextResponse.next();
