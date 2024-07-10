7daaa9f722fc6abd77549a013b3eeff92c82ac89
Rico Putra Pradana <pradanaricoputra@gmail.com>
2024-07-10 16:26:11 +0700

    chore: add an error page just for learning purpose

---
+++ src/app/signup/error.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from 'react'
+
+const Error: React.FC = () => {
+  return (
+    <div>Signup Error Page</div>
+  )
+}
+
+export default Error
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import SignupForm from "@/components/SignupForm";
 
 export default async function Page() {
+
+	// * if you uncomment this line, the error page will be shown
+	// * and the signup form will not be shown
+	// * the error page is defined in `/src/app/signup/error.tsx`
+	// throw new Error("HAHAHA");
+
 	return (
 		<main className="flex justify-center items-center h-screen">
 			<SignupForm />
