c55c5d0e9449ca38c2a1864e7fd2c59c8d05cc53
Rico Putra Pradana <pradanaricoputra@gmail.com>
2024-07-11 07:09:15 +0700

    feat: adjust the middleware to make sure protected routes cannot be accessed if the user is not authenticated yet

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,12 +5,30 @@ import EnumRoutes from "./enums/EnumRoutes";
 export function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
 
+  const isAuthenticated = !!req.cookies.get("auth_session");
+  const PUBLIC_ROUTES = [EnumRoutes.LOGIN, EnumRoutes.SIGNUP];
+  const PROTECTED_ROUTES = [
+    EnumRoutes.DASHBOARD,
+    EnumRoutes.TRANSACTIONS,
+    EnumRoutes.CATEGORIES,
+    EnumRoutes.BUDGETS,
+    EnumRoutes.WALLETS,
+    EnumRoutes.GOALS
+  ];
+
+
   /**
    * Redirects the user to the authenticated page if they are already authenticated.
    */
-  const PROTECTED_ROUTES = [EnumRoutes.LOGIN, EnumRoutes.SIGNUP, "/"];
-  if (PROTECTED_ROUTES.includes(pathname as EnumRoutes) && req.cookies.get("auth_session")) {
+  if (isAuthenticated && ["/", ...PUBLIC_ROUTES].includes(pathname)) {
     return NextResponse.redirect(new URL(EnumRoutes.DASHBOARD, req.url));
+  }
+
+  /**
+   * Redirecs the user to the login page if they are not authenticated.
+   */
+  if (!isAuthenticated && ["/", ...PROTECTED_ROUTES].includes(pathname)) {
+    return NextResponse.redirect(new URL(EnumRoutes.LOGIN, req.url));
   }
 
   return NextResponse.next();
