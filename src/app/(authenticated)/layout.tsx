78f4b3a124700306e21138de30d1aa9d50ada193
Rico Putra Pradana <pradanaricoputra@gmail.com>
2024-08-20 21:34:51 +0700

    [FT-51] feat: revamp the side navbar

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -12,24 +12,8 @@ export default function AuthenticatedLayout({
 }) {
   return (
     <>
-      <div className="flex h-screen bg-[#212529] p-4 pl-0">
-        <aside className="flex flex-col w-64">
-          <div className="flex gap-2 justify-center items-center p-4">
-            <Image
-              src="/logo.png"
-              alt="logo"
-              width={40}
-              height={40}
-              className="rounded-full"
-            />
-            <h1 className="text-white text-2xl font-bold">
-              FinTrack
-            </h1>
-          </div>
-          <div className="flex-1">
-            <SideNav />
-          </div>
-        </aside>
+      <div className="flex h-screen">
+        <SideNav />
         <div className="flex-1 flex flex-col bg-white rounded-sm">
           <Header />
           <main className="flex-1 p-4">
--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -6,31 +6,45 @@ import { usePathname } from 'next/navigation'
 
 import { cn } from '@/lib/utils'
 import EnumRoutes from "@/enums/EnumRoutes"
+import { LayoutDashboard, PieChart, Tag, CreditCard, Goal, ArrowLeftRight } from 'lucide-react';
+import Image from 'next/image';
 
-const menus = [
+interface IMenuItem {
+  href: string;
+  label: string;
+  icon: JSX.Element;
+}
+
+const MAIN_MENU: IMenuItem[] = [
   {
     href: EnumRoutes.DASHBOARD,
     label: 'Dashboard',
+    icon: <LayoutDashboard className="h-5 w-5" />
   },
   {
     href: EnumRoutes.TRANSACTIONS,
     label: 'Transactions',
+    icon: <ArrowLeftRight className="h-5 w-5" />
   },
   {
     href: EnumRoutes.BUDGETS,
     label: 'Budgets',
+    icon: <PieChart className="h-5 w-5" />
   },
   {
     href: EnumRoutes.CATEGORIES,
     label: 'Categories',
+    icon: <Tag className="h-5 w-5" />
   },
   {
     href: EnumRoutes.WALLETS,
     label: 'Wallets',
+    icon: <CreditCard className="h-5 w-5" />
   },
   {
     href: EnumRoutes.GOALS,
     label: 'Goals',
+    icon: <Goal className="h-5 w-5" />
   },
 ]
 
@@ -38,25 +52,49 @@ const SideNav: React.FC = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="flex flex-col justify-between h-full md:px-4 sm:p-0">
-      <ul>
-        {menus.map((menu) => (
-          <li key={menu.href}>
-            <Link
-              href={menu.href}
-              className={cn(
-                "flex items-center h-10 my-2 p-4 rounded-md",
-                "text-white text-sm font-medium",
-                {
-                  "bg-[#343a40]": pathname === menu.href
-                }
-              )}>
-              {menu.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <aside className="flex flex-col w-64 bg-gray-100">
+      <div className="min-h-20 px-4">
+        <div className="h-full py-4 border-b border-gray-300 flex gap-3 justify-center items-center">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            width={40}
+            height={40}
+          />
+          <h1 className=" text-xl font-bold">
+            FinTrack
+          </h1>
+        </div>
+      </div>
+
+      <nav className="mt-3 flex-1 flex flex-col justify-between h-full">
+        <ul className="px-4">
+          {MAIN_MENU.map((menu) => (
+            <li key={menu.href}>
+              <Link
+                href={menu.href}
+                className={cn(
+                  'flex items-center gap-3 p-2 mb-1 rounded-lg hover:bg-white hover:shadow-md transition-shadow duration-200',
+                  pathname === menu.href ? 'bg-white shadow-md transition-shadow duration-200' : ''
+                )}
+              >
+                <span className={cn(
+                  pathname === menu.href ? 'text-blue-500' : 'text-gray-800',
+                )}>
+                  {menu.icon}
+                </span>
+                <span className={cn(
+                  "text-sm text-gray-800",
+                  pathname === menu.href ? 'font-semibold' : ''
+                )}>
+                  {menu.label}
+                </span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
   )
 }
 
